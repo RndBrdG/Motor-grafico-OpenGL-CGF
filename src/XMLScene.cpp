7c79db617fd36e64fcf70e72656fb0b36e9f30e3
Sukigu <Sukigu@users.noreply.github.com>
2014-10-01 16:37:38 +0100

    Alteração na leitura das câmaras

--- src/XMLScene.cpp
+++ src/XMLScene.cpp
@@ -117,49 +117,58 @@ XMLScene::XMLScene(char *filename)
 		else
 			printf("Error parsing camera\n");
 
-		// Tag "perspective"
-		TiXmlElement* perspElement = camElement->FirstChildElement("perspective");
-		if (perspElement)
-		{
-			char *id = NULL, *position = NULL, *target = NULL;
-			float near, far, angle, posX, posY, posZ, targetX, targetY, targetZ;
+		// Procurar câmaras.
+		TiXmlElement* camInstElement = camElement->FirstChildElement();
 
-			id = (char *)perspElement->Attribute("id");
-			position = (char *)perspElement->Attribute("pos");
-			target = (char *)perspElement->Attribute("target");
+		do {
+			if (camInstElement->ValueTStr() == "perspective") { // Tag "perspective"
+				TiXmlElement* perspElement = camElement->FirstChildElement("perspective");
+				if (perspElement)
+				{
+					char *id = NULL, *position = NULL, *target = NULL;
+					float near, far, angle, posX, posY, posZ, targetX, targetY, targetZ;
 
-			if (id != NULL && (perspElement->QueryFloatAttribute("near", &near) == TIXML_SUCCESS) &&
-				(perspElement->QueryFloatAttribute("far", &far) == TIXML_SUCCESS) && (perspElement->QueryFloatAttribute("angle", &angle) == TIXML_SUCCESS)
-				&& (position != NULL && sscanf(position, "%f %f %f", &posX, &posY, &posZ) == 3) && (target != NULL && sscanf(target, "%f %f %f", &targetX, &targetY, &targetZ) == 3)){
-				printf("\n\n< - - - Values of perspective - - - - >\n");
-				printf(" Id : %s", id);
-				printf(" Valores >  %f %f %f %f %f %f %f %f", near, far, angle, posX, posY, posZ, targetX, targetY, targetZ);
+					id = (char *)perspElement->Attribute("id");
+					position = (char *)perspElement->Attribute("pos");
+					target = (char *)perspElement->Attribute("target");
+
+					if (id != NULL && (perspElement->QueryFloatAttribute("near", &near) == TIXML_SUCCESS) &&
+						(perspElement->QueryFloatAttribute("far", &far) == TIXML_SUCCESS) && (perspElement->QueryFloatAttribute("angle", &angle) == TIXML_SUCCESS)
+						&& (position != NULL && sscanf(position, "%f %f %f", &posX, &posY, &posZ) == 3) && (target != NULL && sscanf(target, "%f %f %f", &targetX, &targetY, &targetZ) == 3)){
+						printf("\n< - - - Values of perspective - - - - >\n");
+						printf(" Id : %s\n", id);
+						printf(" Valores >  %f %f %f %f %f %f %f %f\n", near, far, angle, posX, posY, posZ, targetX, targetY, targetZ);
+					}
+					else
+						printf("Error reading perspective tag\n");
+				}
 			}
-			else
-				printf("There was an error reading perspective tag ");
-		}
+			else if (camInstElement->ValueTStr() == "ortho") { // Tag "ortho"
+				TiXmlElement* orthElement = camElement->FirstChildElement("ortho");
+				if (orthElement)
+				{
+					char *id = NULL, *dir = NULL;
+					float near, far, left, right, top, bottom;
 
-		// Tag "ortho"
-		TiXmlElement* orthElement = camElement->FirstChildElement("ortho");
-		if (orthElement)
-		{
-			char *id = NULL, *dir = NULL;
-			float near, far, left, right, top, bottom;
+					id = (char *)orthElement->Attribute("id");
+					dir = (char *)orthElement->Attribute("direction");
 
-			id = (char *)orthElement->Attribute("id");
-			dir = (char *)orthElement->Attribute("direction");
-
-			if (id != NULL && dir != NULL && (orthElement->QueryFloatAttribute("near", &near) == TIXML_SUCCESS) && (orthElement->QueryFloatAttribute("left", &left) == TIXML_SUCCESS)
-				&& (orthElement->QueryFloatAttribute("far", &far) == TIXML_SUCCESS) && (orthElement->QueryFloatAttribute("right", &right) == TIXML_SUCCESS)
-				&& (orthElement->QueryFloatAttribute("top", &top) == TIXML_SUCCESS) && (orthElement->QueryFloatAttribute("bottom", &bottom) == TIXML_SUCCESS)){
-				printf("\n\n< - - - Values of ortho - - - - >\n");
-				printf(" Id : %s \n", id);
-				printf(" Dir : %s \n", dir);
-				printf(" Valores >>  %f - %f - %f - %f - %f - %f \n", near, far, left, right, top, bottom);
+					if (id != NULL && dir != NULL && (orthElement->QueryFloatAttribute("near", &near) == TIXML_SUCCESS) && (orthElement->QueryFloatAttribute("left", &left) == TIXML_SUCCESS)
+						&& (orthElement->QueryFloatAttribute("far", &far) == TIXML_SUCCESS) && (orthElement->QueryFloatAttribute("right", &right) == TIXML_SUCCESS)
+						&& (orthElement->QueryFloatAttribute("top", &top) == TIXML_SUCCESS) && (orthElement->QueryFloatAttribute("bottom", &bottom) == TIXML_SUCCESS)){
+						printf("\n\n< - - - Values of ortho - - - - >\n");
+						printf(" Id : %s\n", id);
+						printf(" Dir : %s\n", dir);
+						printf(" Valores >>  %f - %f - %f - %f - %f - %f\n", near, far, left, right, top, bottom);
+					}
+					else
+						printf("Error reading ortho tag\n");
+				}
 			}
-			else
-				printf(" - - -  -> There was an error reading perspective tag ");
-		}
+			else {
+				printf("Unknown camera tag found\n");
+			}
+		} while (camInstElement = camInstElement->NextSiblingElement());
 	}
 
 	/*
@@ -224,6 +233,8 @@ XMLScene::XMLScene(char *filename)
 
 	}
 	*/
+
+	printf("\n\n");
 }
 
 XMLScene::~XMLScene()

46bfbeb7c4374708324c674b079893ef679e7ee0
Sukigu <Sukigu@users.noreply.github.com>
2014-11-19 21:53:09 +0000

    Alterações à Flag

--- include/Flag.h
+++ include/Flag.h
@@ -2,13 +2,16 @@
 #define FLAG_H
 
 #include <string>
+#include "CGFshader.h"
 #include "Primitiva.h"
 
 class Flag : public Primitiva {
 protected:
 	std::string texture;
+	CGFshader* shader;
 public:
 	Flag(std::string);
+	~Flag();
 
 	void draw(float, float);
 	void draw();
--- src/Flag.cpp
+++ src/Flag.cpp
@@ -1,3 +1,5 @@
+/*#include <GL/glew.h>
+#include <GL/glut.h>*/
 #include "Flag.h"
 
 Flag::Flag(std::string texture) {
@@ -9,5 +11,8 @@ void Flag::draw(float textS, float textT) {
 }
 
 void Flag::draw() {
-
+/*	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	char* vertexShaderName = "shader.vert";
+	int vertexShaderNameLen = sizeof(vertexShaderName);
+	glShaderSource(vertexShader, 1, &vertexShaderName, &vertexShaderNameLen);*/
 }
